2f9eae8a970206e2b3fb95248b2ba47ac71a5af8
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-05 08:43:36 +0530

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,29 +1,18 @@
-provider "azurerm" {
-  features {}
-}
+provider "azurerm" { features {} }
 
-# ------------------------------
-# Resource Group
-# ------------------------------
 resource "azurerm_resource_group" "rg" {
-  name     = "tf-rg"
+  name     = "tf-rg-dev"
   location = "WestUS2"
 }
 
-# ------------------------------
-# Storage Account
-# ------------------------------
 resource "azurerm_storage_account" "storage" {
-  name                     = "tfstoragedemo177"
+  name                     = "tfstoragedemo177dev"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# ------------------------------
-# Storage Containers
-# ------------------------------
 resource "azurerm_storage_container" "input_container" {
   name                  = "input"
   storage_account_name  = azurerm_storage_account.storage.name
@@ -36,11 +25,8 @@ resource "azurerm_storage_container" "output_container" {
   container_access_type = "private"
 }
 
-# ------------------------------
-# Data Factory
-# ------------------------------
 resource "azurerm_data_factory" "adf" {
-  name                = "adfdemo177"
+  name                = "adfdemo177dev"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
