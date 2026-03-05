fd53412a5936669dcc03d86283d49395424192f8
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-05 08:37:11 +0530

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -2,13 +2,17 @@ provider "azurerm" {
   features {}
 }
 
+# ------------------------------
 # Resource Group
+# ------------------------------
 resource "azurerm_resource_group" "rg" {
   name     = "tf-rg"
   location = "WestUS2"
 }
 
-# Storage Account (for state + CSV)
+# ------------------------------
+# Storage Account
+# ------------------------------
 resource "azurerm_storage_account" "storage" {
   name                     = "tfstoragedemo177"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -17,7 +21,24 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 }
 
+# ------------------------------
+# Storage Containers
+# ------------------------------
+resource "azurerm_storage_container" "input_container" {
+  name                  = "input"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "output_container" {
+  name                  = "output"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+# ------------------------------
 # Data Factory
+# ------------------------------
 resource "azurerm_data_factory" "adf" {
   name                = "adfdemo177"
   location            = azurerm_resource_group.rg.location
