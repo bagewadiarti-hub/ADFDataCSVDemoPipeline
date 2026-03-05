3d288af73588a9cc816871ab2bdfcab6be85f3ea
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-05 08:37:36 +0530

    Update outputs.tf

--- env/dev/outputs.tf
+++ env/dev/outputs.tf
@@ -5,3 +5,15 @@ output "resource_group" {
 output "data_factory_name" {
   value = azurerm_data_factory.adf.name
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "input_container_name" {
+  value = azurerm_storage_container.input_container.name
+}
+
+output "output_container_name" {
+  value = azurerm_storage_container.output_container.name
+}
