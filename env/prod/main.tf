efab2edcb2c3dad4cc1e7a527e2377c58820aa78
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-05 08:45:43 +0530

    Create main.tf

---
+++ env/prod/main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" { features {} }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-rg-prod"
+  location = "WestUS2"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfstoragedemo177prod"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "input_container" {
+  name                  = "input"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "output_container" {
+  name                  = "output"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_data_factory" "adf" {
+  name                = "adfdemo177prod"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
